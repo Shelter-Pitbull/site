8bc6c02d16f0bfba22e9de1d7e5cc3a805998dc0
Ponsipi <fredponsipi@gmail.com>
2021-04-10 06:36:27 -0700

    Auto-generated commit

---
+++ assets/vendor/layerslider/js/layerslider.load.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function () {
+	"use strict";
+
+	// Calling LayerSlider 
+		
+        $('#slider').layerSlider({
+            responsive: true,
+			fitScreenWidth:false,
+            responsiveUnder: 1280,
+            layersContainer: 1280,
+            skin: 'v6',
+            hoverPrevNext: false,
+            skinsPath: 'assets/vendor/layerslider/skins/',
+            autoStart: true,
+			autoPlayVideos : false
+        });
+
+		
+ });
